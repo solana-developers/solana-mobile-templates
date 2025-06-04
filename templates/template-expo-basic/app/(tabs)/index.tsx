f2fe659903a2e439ff751db0e21048f9be89f14d
Bram Borggreve <beeman@beeman.nl>
2025-06-04 09:49:44 +0200

    feat: add template-expo-basic

---
+++ templates/template-expo-basic/app/(tabs)/index.tsx
@@ -0,0 +1,55 @@
+import { Image } from 'expo-image';
+import { StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#9945FF', dark: '#9945FF' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/solana-gradient.png')}
+          style={{
+            height: 316,
+            width: 518,
+            bottom: 0,
+            left: 0,
+            position: 'absolute',
+          }}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Solana Mobile!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 1: Eat</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 2: Sleep</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 3: Code</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 4: Repeat</ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+});
